e9fc34a59624162701c16910d5e3a699a3657ae3
Ander Gray <ander.gray@liverpool.ac.uk>
2021-05-03 16:14:17 +0100

    adds test for membership conservatism

--- test/src/FuzzyNumber.jl
+++ test/src/FuzzyNumber.jl
@@ -1,14 +1,50 @@
-@testset "Fuzzy Numbers" begin
-    A = FuzzyNumber(1,2,3);
-    B = FuzzyNumber(2,3,4);
+@testset "Conservative memberships" begin
 
-    @test interval(1,3) ⊆ A.Membership[1]
-    @test interval(2) ⊆ A.Membership[end]
+    Nsamp = 10^4
 
-    C = levelwise(A,B)
+    A = FuzzyNumber(0,1,2, steps = 1000)
 
-    @test  interval(3,7) ⊆ C.Membership[1]
-    @test interval(5) ⊆ C.Membership[end]
+    U1 = Uniform(0,1);
+    U2 = Uniform(1,2);
+
+    rand1 = rand(Nsamp)
+    U1cdf = cdf(U1,rand1);
+
+    rand2 = rand(Nsamp) .+1;
+    U2cdf = 1 .- cdf(U2,rand2);
+
+    @test all(membership.(A, rand1) .>= U1cdf)
+    @test all(membership.(A, rand2) .>= U2cdf)
+
+
+    A = FuzzyNumber(0,1,2, steps = 200)
+
+    U1 = Uniform(0,1);
+    U2 = Uniform(1,2);
+
+    rand1 = rand(Nsamp)
+    U1cdf = cdf(U1,rand1);
+
+    rand2 = rand(Nsamp) .+1;
+    U2cdf = 1 .- cdf(U2,rand2);
+
+    @test all(membership.(A, rand1) .>= U1cdf)
+    @test all(membership.(A, rand2) .>= U2cdf)
+
+
+    A = FuzzyNumber(0,1,2, steps = 10)
+
+    U1 = Uniform(0,1);
+    U2 = Uniform(1,2);
+
+    rand1 = rand(Nsamp)
+    U1cdf = cdf(U1,rand1);
+
+    rand2 = rand(Nsamp) .+1;
+    U2cdf = 1 .- cdf(U2,rand2);
+
+    @test all(membership.(A, rand1) .>= U1cdf)
+    @test all(membership.(A, rand2) .>= U2cdf)
 
 end
 
@@ -24,5 +60,20 @@ end
     @test mass(A,interval(1,3)) == interval(0,1)
     @test mass(A,interval(2)).hi == 1
     @test mass(A,interval(-10, -5)).hi == interval(0)
+
+end
+
+@testset "Levelwise" begin
+
+    A = FuzzyNumber(1,2,3);
+    B = FuzzyNumber(2,3,4);
+
+    @test interval(1,3) ⊆ A.Membership[1]
+    @test interval(2) ⊆ A.Membership[end]
+
+    C = levelwise(A,B)
+
+    @test  interval(3,7) ⊆ C.Membership[1]
+    @test interval(5) ⊆ C.Membership[end]
 
 end
